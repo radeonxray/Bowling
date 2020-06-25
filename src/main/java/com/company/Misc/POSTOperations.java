39bc83ab463cdcdfa71a77f0ec814d533a786db1
radeonxray <ceo_titanic@msn.com>
2020-06-25 15:31:11 +0200

    Updates to Post-method and added support for different api url.
    Due to incompatibility between Ceobackend and the Post-method using HttpURlConnection, I've added a new Post Method that uses the HTTPClient-method, that works with both CeoBackend and SkatBackend.
    Please note that the HttpClient is set to Version 1_1
    
    Updated the way the app is run, so to better support running with and changing between different api URL.
    
    With the sucess of the HTTPClient in POST, I should consider doing the same woth for the GET-method.

--- src/main/java/com/company/Misc/POSTOperations.java
+++ src/main/java/com/company/Misc/POSTOperations.java
@@ -1,5 +1,6 @@
 package com.company.Misc;
 
+import com.company.Connection.APIConnection;
 import com.company.Connection.POSTConnection;
 import com.company.ObjectClasses.POSTObject;
 import org.json.JSONObject;
@@ -8,6 +9,12 @@ import java.io.IOException;
 
 public class POSTOperations {
 
+    private APIConnection apiConn;
+
+    /**
+     * Method for preparing the PostObject and return it as a JSON String
+     * @param postObject Requires a POSTObject
+     * @return (json)String*/
     public String prepareJSONString_toPost(POSTObject postObject){
 
         JSONObject jsonObject = new JSONObject();
@@ -17,13 +24,24 @@ public class POSTOperations {
         return jsonObject.toString();
     }
 
-    public void postToAPI(String postObjectJsonString) throws IOException {
+    /**
+     * Method for Posting the provided JSONString to the provided String apiURL
+     * Note that this method is using the old HttpURLConnection method, which is not compatible with CeoBackend, perhaps due to issues with modern backends
+     * @param apiURL Requires a String containing the api url
+     * @param postObjectJsonString Requires a (json)String with the data to be send*/
+    public void postToAPI_httpurlconnection(String postObjectJsonString, String apiURL) throws IOException {
         POSTConnection postCon = new POSTConnection();
+        postCon.Post_HttpURLConnection(postObjectJsonString, apiURL);
+    }
 
-        postCon.POST(postObjectJsonString);
-
-        // To be removed later!
-        //System.out.println("The following JSON string has been sent: \n" + postObjectJsonString);
+    /**
+     * Method for Posting the provided JSONString to the provided String apiURL
+     * Note that this method is using the java 11 HttpClient method, to better be compatible with both SkatBackendApi and CeoBackendApi
+     * @param apiURL Requires a String containing the api url
+     * @param postObjectJsonString Requires a (json)String with the data to be send*/
+    public void postToAPI_httpclient(String postObjectJsonString, String apiURL) throws IOException, InterruptedException {
+        POSTConnection postCon = new POSTConnection();
+        postCon.Post_HttpClient(postObjectJsonString, apiURL);
     }
 
 
--- src/test/java/com/company/Connection/GETConnectionTest.java
+++ src/test/java/com/company/Connection/GETConnectionTest.java
@@ -9,12 +9,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class GETConnectionTest {
 
     /**
+     * Testing Skat Backend
      * Simple test to show that the GET-method returns a string with content, and is not empty or null
      * */
     @Test
-    void test_getData_get_responseCode() throws IOException {
+    void test_getData_get_responseCode_SkatBackend() throws IOException {
         GETConnection getCon = new GETConnection();
-        String dataString = getCon.getData();
+        APIConnection apiCon = new APIConnection();
+        String dataString = getCon.getData_HttpURLConnection(apiCon.getSkatBackendApi());
+        assertNotNull(dataString);
+    }
+
+    /**
+     * Testing Ceo Backend
+     * Simple test to show that the GET-method returns a string with content, and is not empty or null
+     * */
+    @Test
+    void test_getData_get_responseCode_CeoBackend() throws IOException {
+        GETConnection getCon = new GETConnection();
+        APIConnection apiCon = new APIConnection();
+        String dataString = getCon.getData_HttpURLConnection(apiCon.getCeoBackendApi());
         assertNotNull(dataString);
     }
 }
