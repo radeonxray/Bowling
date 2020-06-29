f32a0746a00a654821d482127261613bb6863954
radeonxray <ceo_titanic@msn.com>
2020-06-29 02:21:24 +0200

    API Update
    Updated the APIConnection for getCeoBackendApi from pointing to a localhost, to the actual Live CEOBackend. This change has also been updated and reflected in the README.md

--- src/main/java/com/company/Connection/APIConnection.java
+++ src/main/java/com/company/Connection/APIConnection.java
@@ -2,7 +2,7 @@ package com.company.Connection;
 
 public class APIConnection {
 
-    private String ceoBackendApi = "http://localhost:8080/bowling/api/points";
+    private String ceoBackendApi = "https://ceolsen.com:8081/bowling/api/points";
     private String skatBackendApi = "http://13.74.31.101/api/points";
 
     /** Method for getting the API URl to the Ceo Backend
